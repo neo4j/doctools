839f1c32c261ba69daa98c2bb4b8d6fb0843e2f8
Anders Nawroth <anders@neotechnology.com>
2012-07-18 00:10:34 +0200

    Start to separate the tools from the content.

--- src/build/copy-images.sh
+++ src/build/copy-images.sh
@@ -2,8 +2,9 @@
 
 srcdir=$1
 imgdir=$2
+path=$3
 
-find "$srcdir" -path '*images' -type d -print | while read i; do 
-  cp -fr "${i}"/* "$imgdir"
+find "${srcdir}" -path ${path} ! -wholename "*target*" -type d -print | while read i; do 
+  cp -fr "${i}"/* "${imgdir}"
 done
 
