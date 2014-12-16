959310412ff3ca23de9dbbe959eef93040afe24f
Anders Nawroth <anders@neotechnology.com>
2014-12-16 10:20:44 +0100

    Syntax highlight Cypher fragments.

--- src/main/resources/js/colorize.js
+++ src/main/resources/js/colorize.js
@@ -20,6 +20,9 @@ CodeMirror.colorize = (function() {
       if (!mode) continue;
       if (mode === "cypher") {
         theme = " cm-s-neo";
+      } else if (mode === "cypher-noexec") {
+        mode = "cypher";
+        theme = " cm-s-neo";
       } else if (mode === "java") {
         mode = "text/x-java";
       } else if (mode === "sql") {
